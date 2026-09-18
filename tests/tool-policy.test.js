786603497ff5cdf67d9e56935536e91f5adb8ba9
Ethen Ching <sadsinger@gmail.com>
2026-09-19 02:56:48 +0800

    test: cover side-effect escalation and fail-closed unknowns

--- tests/tool-policy.test.js
+++ tests/tool-policy.test.js
@@ -4,18 +4,29 @@ const assert = require('assert');
 const {
   TOOL_RISK,
   classifyToolRisk,
+  classifyToolCallRisk,
   requiresExplicitApproval,
   buildToolPolicy
 } = require('../lib/tool-policy');
 
 assert.strictEqual(classifyToolRisk('read_page'), TOOL_RISK.READ_ONLY);
 assert.strictEqual(classifyToolRisk('fill_form_fields'), TOOL_RISK.REVERSIBLE_MUTATION);
+assert.strictEqual(classifyToolRisk('click'), TOOL_RISK.EXTERNAL_SIDE_EFFECT);
 assert.strictEqual(classifyToolRisk('run_js'), TOOL_RISK.HIGH_IMPACT);
+assert.strictEqual(
+  classifyToolCallRisk('type', { submit: true }),
+  TOOL_RISK.EXTERNAL_SIDE_EFFECT
+);
+assert.strictEqual(
+  classifyToolCallRisk('type', { submit: false }),
+  TOOL_RISK.REVERSIBLE_MUTATION
+);
 assert.strictEqual(classifyToolRisk('does-not-exist'), TOOL_RISK.UNKNOWN);
 
 assert.strictEqual(requiresExplicitApproval(TOOL_RISK.READ_ONLY), false);
 assert.strictEqual(requiresExplicitApproval(TOOL_RISK.REVERSIBLE_MUTATION), false);
 assert.strictEqual(requiresExplicitApproval(TOOL_RISK.HIGH_IMPACT), true);
+assert.strictEqual(requiresExplicitApproval(TOOL_RISK.UNKNOWN), true);
 
 assert.deepStrictEqual(buildToolPolicy('run_js'), {
   name: 'run_js',
